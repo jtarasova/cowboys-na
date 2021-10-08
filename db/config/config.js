42b09c63d71abd926326dc0b316dc6c39bc654d5
Julia <tarasova.bmstu@gmail.com>
2021-10-08 11:59:50 +0300

    db added

--- db/config/config.js
+++ db/config/config.js
@@ -16,11 +16,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'lmsaznfibxahhk',
-    password:
-      '131c0c23aa4257849f5d34dcea20ba98a4816f125e1afe33b8d9cae8ac1d7ee1',
-    database: 'd297al2rrbuje4',
-    host: 'ec2-52-17-1-206.eu-west-1.compute.amazonaws.com',
+    username: process.env.PS_USER,
+    password: process.env.PS_PASS,
+    database: process.env.PS_DB,
+    host: process.env.PS_HOST,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
